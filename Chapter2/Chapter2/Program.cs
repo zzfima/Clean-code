ae162bb163f556acf88d94a6fd971240aeb468d4
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2026-07-29 17:35:49 +0300

    rfct

--- Chapter2/Chapter2/Program.cs
+++ Chapter2/Chapter2/Program.cs
@@ -41,28 +41,41 @@ internal class Program
     }
 
     //refactoring 2
-    static Cell[] cells = { new(1, 0), new(4, 1), new(3, 2), new(4, 3) };
+    static Cell[] cells =
+    {
+        new(CellStatus.Empty, 0),
+        new(CellStatus.Flagged, 1),
+        new(CellStatus.Available, 2),
+        new(CellStatus.Flagged, 3)
+    };
 
     public static List<Cell> getFlaggedCellsEx()
     {
         List<Cell> flaggedCells = new List<Cell>();
         foreach (Cell x in cells)
-            if (x.IsStatus(FLAGGED))
+            if (x.IsFlaggedCell())
                 flaggedCells.Add(x);
         return flaggedCells;
     }
 
+    public enum CellStatus
+    {
+        Empty = 1,
+        Available = 3,
+        Flagged = 4
+    }
+
     public class Cell
     {
-        private int _status;
+        private CellStatus _status;
         private int _value;
 
-        public Cell(int status, int value)
+        public Cell(CellStatus status, int value)
         {
             _status = status;
             _value = value;
         }
 
-        internal bool IsStatus(int status) => _status == status;
+        internal bool IsFlaggedCell() => _status == CellStatus.Flagged;
     }
 }
