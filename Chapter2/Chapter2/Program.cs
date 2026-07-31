7e93e3bd83ed740d703cddc9af67c7940225e5f7
zzfima <zzfima@gmail.com>
2026-07-31 09:14:00 +0300

    add tests

--- Chapter2/Chapter2/Program.cs
+++ Chapter2/Chapter2/Program.cs
@@ -1,29 +1,51 @@
 ﻿using System.Diagnostics;
 
-internal class Program
+public class Program
 {
-    static int[][] theList = new int[4][];
+    public static int[][] TheList = new int[4][];
 
-    private static void Main(string[] args)
+    // Test helper method to set up test data
+    public static void SetTestData(int[][] testData)
     {
-        theList[0] = new int[] { 1, 0 };
-        theList[1] = new int[] { 4, 1 };
-        theList[2] = new int[] { 3, 2 };
-        theList[3] = new int[] { 4, 3 };
+        TheList = testData;
+        theBoard = testData;
+    }
 
-        var originalFlaggedCells = getThem();
-        var refactoredFlaggedCells = getFlaggedCells();
-        var finalRefactoredFlaggedCells = ProgramHelpers.getFlaggedCellsEx();
+    // Test helper method to reset to default data
+    public static void ResetToDefaultData()
+    {
+        TheList = new int[4][];
+        TheList[0] = new int[] { 1, 0 };
+        TheList[1] = new int[] { 4, 1 };
+        TheList[2] = new int[] { 3, 2 };
+        TheList[3] = new int[] { 4, 3 };
+        theBoard = TheList;
+    }
+
+    public static void RunDemo()
+    {
+        ResetToDefaultData();
+
+        var originalFlaggedCells = GetThem();
+        var refactoredFlaggedCells = GetFlaggedCells();
+        var finalRefactoredFlaggedCells = ProgramHelpers.GetFlaggedCellsEx();
 
         Console.WriteLine("clean code first refactoring");
     }
 
+    private static void Main(string[] args)
+    {
+        RunDemo();
+    }
+
     //before
-    public static List<int[]> getThem()
+    public static List<int[]> GetThem()
     {
         List<int[]> list1 = new List<int[]>();
-        foreach (int[] x in theList)
-            if (x[0] == 4)
+        if (TheList == null) return list1;
+        
+        foreach (int[] x in TheList)
+            if (x != null && x[0] == 4)
                 list1.Add(x);
         return list1;
     }
@@ -31,13 +53,15 @@ internal class Program
     //refactoring 1
     static int FLAGGED = 4;
     static int STATUS_VALUE = 0;
-    static int[][] theBoard = theList;
+    static int[][] theBoard = TheList;
 
-    public static List<int[]> getFlaggedCells()
+    public static List<int[]> GetFlaggedCells()
     {
         List<int[]> flaggedCells = new List<int[]>();
+        if (theBoard == null) return flaggedCells;
+        
         foreach (int[] x in theBoard)
-            if (x[STATUS_VALUE] == FLAGGED)
+            if (x != null && x[STATUS_VALUE] == FLAGGED)
                 flaggedCells.Add(x);
         return flaggedCells;
     }
