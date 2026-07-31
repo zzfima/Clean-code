d10be8ad49dbe7b3de98425e617e49cb88da8a7e
zzfima <zzfima@gmail.com>
2026-07-31 08:42:04 +0300

    extract class

--- Chapter2/Chapter2/Program.cs
+++ Chapter2/Chapter2/Program.cs
@@ -13,7 +13,7 @@ internal class Program
 
         var originalFlaggedCells = getThem();
         var refactoredFlaggedCells = getFlaggedCells();
-        var finalRefactoredFlaggedCells = getFlaggedCellsEx();
+        var finalRefactoredFlaggedCells = ProgramHelpers.getFlaggedCellsEx();
     }
 
     //before
@@ -38,44 +38,5 @@ internal class Program
             if (x[STATUS_VALUE] == FLAGGED)
                 flaggedCells.Add(x);
         return flaggedCells;
-    }
-
-    //refactoring 2
-    static Cell[] cells =
-    {
-        new(CellStatus.Empty, 0),
-        new(CellStatus.Flagged, 1),
-        new(CellStatus.Available, 2),
-        new(CellStatus.Flagged, 3)
-    };
-
-    public static List<Cell> getFlaggedCellsEx()
-    {
-        List<Cell> flaggedCells = new List<Cell>();
-        foreach (Cell x in cells)
-            if (x.IsFlaggedCell())
-                flaggedCells.Add(x);
-        return flaggedCells;
-    }
-
-    public enum CellStatus
-    {
-        Empty = 1,
-        Available = 3,
-        Flagged = 4
-    }
-
-    public class Cell
-    {
-        private CellStatus _status;
-        private int _value;
-
-        public Cell(CellStatus status, int value)
-        {
-            _status = status;
-            _value = value;
-        }
-
-        internal bool IsFlaggedCell() => _status == CellStatus.Flagged;
     }
 }
---
+++ Chapter2/Chapter2/ProgramHelpers.cs
@@ -0,0 +1,42 @@
+﻿internal static class ProgramHelpers
+{
+
+    //refactoring 2
+    static Cell[] cells =
+    {
+        new(CellStatus.Empty, 0),
+        new(CellStatus.Flagged, 1),
+        new(CellStatus.Available, 2),
+        new(CellStatus.Flagged, 3)
+    };
+
+    public static List<Cell> getFlaggedCellsEx()
+    {
+        List<Cell> flaggedCells = new List<Cell>();
+        foreach (Cell x in cells)
+            if (x.IsFlaggedCell())
+                flaggedCells.Add(x);
+        return flaggedCells;
+    }
+
+    public enum CellStatus
+    {
+        Empty = 1,
+        Available = 3,
+        Flagged = 4
+    }
+
+    public class Cell
+    {
+        private CellStatus _status;
+        private int _value;
+
+        public Cell(CellStatus status, int value)
+        {
+            _status = status;
+            _value = value;
+        }
+
+        internal bool IsFlaggedCell() => _status == CellStatus.Flagged;
+    }
+}
