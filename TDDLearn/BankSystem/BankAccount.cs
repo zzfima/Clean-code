be9ffe66841b4cbbfa7e7c559320d33a0f438048
zzfima <zzfima@gmail.com>
2026-08-07 14:16:48 +0300

    add tests

---
+++ TDDLearn/BankSystem/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿namespace BankSystem
+{
+    public class BankAccount : IAccount
+    {
+        private decimal _balance;
+
+        public BankAccount(int startingBalance)
+        {
+            _balance = startingBalance;
+        }
+
+        public decimal Balance => _balance;
+
+        public void Deposit(decimal amount) => _balance += amount;
+
+        public void Withdraw(decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDDLearn/BankTest/UnitTestBankAccount.cs
@@ -0,0 +1,42 @@
+﻿using BankSystem;
+
+namespace BankTest
+{
+    public class UnitTestBankAccount
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestInitialBalanceZero()
+        {
+            IAccount account = new BankAccount(0);
+            Assert.That(account.Balance == 0);
+        }
+
+        [Test]
+        public void TestInitialBalanceTenThousand()
+        {
+            IAccount account = new BankAccount(10000);
+            Assert.That(account.Balance == 10000);
+        }
+
+        [Test]
+        public void TestDepositTenThousand()
+        {
+            IAccount account = new BankAccount(123);
+            account.Deposit(10000);
+            Assert.That(account.Balance == 10123);
+        }
+
+        [Test]
+        public void TestDepositTwentyThousand()
+        {
+            IAccount account = new BankAccount(123);
+            account.Deposit(20000);
+            Assert.That(account.Balance == 20123);
+        }
+    }
+}
